652cf0cadcc625316e69a9d072e585acd6ec94f1
unatic <38244346+unatic@users.noreply.github.com>
2025-10-04 05:41:28 +0900

    Build the admins for the Tweet and Like models.

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
-
+from .models import Tweet, Like
 # Register your models here.
+
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "payload",
+        "user",
+        "updated_at",
+    )
+
+    list_filter = (
+        "created_at",
+        "updated_at",
+    )
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "user",
+        "tweet",
+        "updated_at",
+    )
+
+    list_filter = (
+        "created_at",
+        "updated_at",
+    )
+
--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.conf import settings
+from common.models import CommonModel
 
-# Create your models here.
+class Tweet(CommonModel):
+
+    """Tweet Model Definition"""
+
+    payload = models.CharField(
+        max_length=180,
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self) -> str:
+        return self.payload
+
+class Like(CommonModel):
+
+    """Like Model Definition"""
+
+    tweet = models.ForeignKey(
+        "tweets.Tweet",
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self) -> str:
+        return str(self.user) + " Likes " + str(self.tweet)
