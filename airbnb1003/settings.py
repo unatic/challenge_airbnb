1d4a36f85c01793d91001d670f9d03742a666869
unatic <38244346+unatic@users.noreply.github.com>
2025-10-06 13:19:55 +0900

    replaced with custom user app

--- airbnb1003/settings.py
+++ airbnb1003/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "common.apps.CommonConfig",
-    "tweets.apps.TweetsConfig"
+    "tweets.apps.TweetsConfig",
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "users.User"
