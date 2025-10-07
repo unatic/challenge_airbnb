7f5c7d6dd1379642d2547dacb56107443dda65df
unatic <38244346+unatic@users.noreply.github.com>
2025-10-07 21:22:43 +0900

    Power Admin Challenge

--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
+## 10/6 Power Admin Challenge
+
+![10/7 Power Admin challenge](./challenge_screenshots/challenge1007.png)
+
+## 10/6 ORM Challenge
+
 ![10/6 ORM challenge](./challenge_screenshots/challenge1006.png)
--- tweets/admin.py
+++ tweets/admin.py
@@ -2,6 +2,28 @@ from django.contrib import admin
 from .models import Tweet, Like
 # Register your models here.
 
+
+class WordFilter(admin.SimpleListFilter):
+
+    title = "Filter by Keyword"
+    parameter_name = "keyword"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("include", "Contain Elon Musk"),
+            ("exclude", "Not Cotain Elon Musk"),
+        ]
+
+    def queryset(self, request, tweets):
+        keyword = self.value()
+        if keyword:
+            if keyword == 'include':
+                return tweets.filter(payload__icontains='Elon Musk')
+            elif keyword == 'exclude':
+                return tweets.exclude(payload__icontains='Elon Musk')
+        else:
+            return tweets
+
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
 
@@ -12,9 +34,14 @@ class TweetAdmin(admin.ModelAdmin):
         "like_count"
     )
 
+    search_fields = (
+        "payload",
+        "user__username",
+    )
+
     list_filter = (
+        WordFilter,
         "created_at",
-        "updated_at",
     )
 
     def like_count(self, tweet):
@@ -29,8 +56,11 @@ class LikeAdmin(admin.ModelAdmin):
         "updated_at",
     )
 
+    search_fields = (
+        "user__username",
+    )
+
     list_filter = (
         "created_at",
-        "updated_at",
     )
 
